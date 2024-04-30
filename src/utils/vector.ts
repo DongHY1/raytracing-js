32048425cb1d21282a2d7e406fea2dfbf3ce630d
David Dong <hi@daviddong.me>
2024-04-30 23:19:00 +0800

    chore: update type

--- src/utils/vector.ts
+++ src/utils/vector.ts
@@ -1,37 +1,42 @@
-import { Vector } from "../types";
+import type { Vector } from "../types";
 
 export function addition(v1: Vector, v2: Vector): Vector {
-  return { x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z };
+	return { x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z };
 }
 
 export function subtract(v1: Vector, v2: Vector): Vector {
-  return { x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z };
+	return { x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z };
 }
 export function dotProduct(v1: Vector, v2: Vector) {
-  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 export function crossProduct(v: Vector, index: number) {
-  return { x: v.x * index, y: v.y * index, z: v.z * index };
+	return { x: v.x * index, y: v.y * index, z: v.z * index };
 }
 
 export function magnitude(v: Vector): number {
-  return Math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2);
+	return Math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2);
 }
 
 export function normalize(v: Vector): Vector {
-  const len = magnitude(v);
-  if (len === 0) {
-    throw new Error("Cannot normalize a zero-length vector");
-  }
-  return { x: v.x / len, y: v.y / len, z: v.z / len };
+	const len = magnitude(v);
+	if (len === 0) {
+		throw new Error("Cannot normalize a zero-length vector");
+	}
+	return { x: v.x / len, y: v.y / len, z: v.z / len };
 }
 
 export function computeNormal(
-  v: Vector,
-  center: Vector = { x: 0, y: 0, z: 0 }
+	v: Vector,
+	center: Vector = { x: 0, y: 0, z: 0 },
 ): Vector {
-  // Subtract the center from v to get the vector pointing from the center to the point
-  const fromCenterToPoint = subtract(v, center);
-  // Normalize this vector to get the unit normal vector
-  return normalize(fromCenterToPoint);
+	// Subtract the center from v to get the vector pointing from the center to the point
+	const fromCenterToPoint = subtract(v, center);
+	// Normalize this vector to get the unit normal vector
+	return normalize(fromCenterToPoint);
+}
+
+// 角度转换为弧度
+export function degreesToRadians(degrees: number) {
+	return (degrees * Math.PI) / 180;
 }
